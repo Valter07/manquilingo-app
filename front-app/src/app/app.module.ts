de836f8940b625ccc1f34ab44a3fdc5141a030a0
Valter07 <zuliani13@hotmail.com>
2020-05-18 18:47:55 -0500

    añadiendo footer

--- front-app/src/app/app.module.ts
+++ front-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RouterModule, Routes } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
     
   ],
   imports: [
