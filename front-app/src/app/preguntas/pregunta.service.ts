46ac2941ad49b226c5a9c84ae37e51896e0197a6
Valter07 <zuliani13@hotmail.com>
2020-05-19 01:30:41 -0500

    f

--- front-app/src/app/preguntas/pregunta.service.ts
+++ front-app/src/app/preguntas/pregunta.service.ts
@@ -8,11 +8,11 @@ import { map, delay } from 'rxjs/operators';
 import { of } from 'rxjs';
 
 
-@Injectable(
- // providedIn: 'root'
-)
+@Injectable({
+ providedIn: 'root'
+})
 export class PreguntaService {
-  private urlEndPoint:string = 'http://localhost:8080/api/preguntas';
+  private urlEndPoint:string = 'http://localhost:8080/api/pregunta';
 
   constructor(private http: HttpClient){ }
 
@@ -20,7 +20,7 @@ export class PreguntaService {
   getPreguntas(): Observable<pregunta[]>{
     return this.http.get(this.urlEndPoint).pipe(
       map(response => response as pregunta[])
-    );
+    )
   }
 
 }
